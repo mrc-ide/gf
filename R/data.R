ce4a777a0245d8814a9ca0db5e3f4b696703b332
Peter Winskill <p.winskill@imperial.ac.uk>
2021-05-05 09:21:07 +0100

    proportion plasmodium falciparum

--- R/data.R
+++ R/data.R
@@ -5,11 +5,15 @@
 #'  \href{https://gh.bmj.com/content/2/2/e000176}{Patouillard _et al_, 2017} and an updated costing review (Ehler _et al_, in prep).
 "unit_costs"
 
-
-#' Unit costs
+#' Treatment unit costs
 #'
 #' A dataset containing the age- and country- specific (treatment) unit costs. 
 #' These were sources from a combination of costing from the original GTS
 #'  \href{https://gh.bmj.com/content/2/2/e000176}{Patouillard _et al_, 2017} and an updated costing review (Ehler _et al_, in prep).
 #'  and the WHOCHOICE data base \href{https://www.who.int/choice/cost-effectiveness/en/}{WHO CHOICE}.
 "treatment_unit_costs"
+
+#' Proportion pf
+#' 
+#' Proportion of cases that are _plasmodium falciparum_. Sourced from the WHO GMP. Proportion is assumed constant in the future.
+"ppf"
