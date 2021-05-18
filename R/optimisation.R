471d545d973c550ba8113ae3c7efe258d7eb8f64
Peter Winskill <p.winskill@imperial.ac.uk>
2021-05-18 13:19:44 +0100

    Add dominant solution filtering to optimisation data prep

--- R/optimisation.R
+++ R/optimisation.R
@@ -2,7 +2,8 @@
 #'
 #' @param processed_output Processed model output
 #'
-#' @return Data for use in optimiation
+#' @return Data for use in optimisation
+#' @export
 create_optimisation_data <- function(processed_output){
   processed_output %>%
     dplyr::filter(.data$year %in% 2024:2026) %>%
@@ -11,5 +12,10 @@ create_optimisation_data <- function(processed_output){
                      deaths = sum(.data$deaths),
                      cost = sum(.data$total_cost)) %>%
     dplyr::ungroup() %>%
-    dplyr::mutate(y = (.data$cases * (sum(.data$deaths) / sum(.data$cases))) + .data$deaths)
+    dplyr::mutate(y = (.data$cases * (sum(.data$deaths) / sum(.data$cases))) + .data$deaths) %>%
+    dplyr::group_by(.data$NAME_0, .data$NAME_1, .data$NAME_2, .data$ur, .data$pre) %>%
+    # Select only dominant solutions
+    dplyr::arrange(.data$cost, .by_group = TRUE) %>%
+    dplyr::filter(y == cummin(.data$y)) %>%
+    dplyr::ungroup()
 }
