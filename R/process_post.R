854ed171274025080e9d8bf1c1c10350c22c1231
Peter Winskill <p.winskill@imperial.ac.uk>
2021-06-01 10:50:39 +0100

    Pruning processed data earlier to prevent malloc issues

--- R/process_post.R
+++ R/process_post.R
@@ -13,5 +13,27 @@ post_process <- function(model_output, coverage_input){
     epi_post_processing() %>%
     commodities_and_services() %>%
     component_costs() %>%
-    category_costs()
+    category_costs() %>%
+    prune()
+}
+
+#' Keep only required columns - helps for memory issues with large countries
+#'
+#' @param x model_output Model output
+prune <- function(x){
+  x %>%
+    dplyr::select(.data$ISO, .data$NAME_0, .data$NAME_1, .data$NAME_2, .data$ur,
+                .data$pre, .data$replenishment, .data$post,
+                .data$year,
+                .data$age_lower, .data$age_upper,
+                .data$par,
+                .data$cases, .data$cases_lower, .data$cases_upper,
+                .data$deaths, .data$deaths_lower, .data$deaths_upper,
+                .data$prev,
+                .data$life_years, .data$life_years_lower, .data$life_years_upper,
+                .data$dalys, .data$dalys_lower, .data$dalys_upper,
+                dplyr::contains("coverage"),
+                .data$net_cost, .data$irs_cost, .data$smc_cost, .data$ipti_cost,
+                .data$rtss_cost, .data$diagnostic_and_treatment_cost,
+                .data$surveillance_cost, .data$elimination_cost, .data$total_cost) 
 }
