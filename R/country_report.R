088afe4531b637d305e0a81a46750ad89f52067c
Peter Winskill <p.winskill@imperial.ac.uk>
2021-07-08 14:11:43 +0100

    More country report diagnostic plots

--- R/country_report.R
+++ R/country_report.R
@@ -1,15 +1,17 @@
 #' Create country summary report
 #'
 #' @param gf_optimised Optimised data for a country
+#' @param gts Country level annual summary from the GTS for a country
 #' @param output_file Report output address
 #'
 #' @export
-render_report <- function(gf_optimised, output_file) {
+render_report <- function(gf_optimised, gts, output_file) {
   rmd_file <- system.file("country_report.Rmd", package = "gf")
   rmarkdown::render(
     rmd_file, 
     params = list(
-      gf_optimised = gf_optimised
+      gf_optimised = gf_optimised,
+      gts = gts
     ),
     output_file = output_file
   )
