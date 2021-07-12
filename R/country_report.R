5ad69bb4e9ef3fac9c8950c62e7af3dc62c9cb1c
Peter Winskill <p.winskill@imperial.ac.uk>
2021-07-12 10:44:56 +0100

    Tweak report for new output processing

--- R/country_report.R
+++ R/country_report.R
@@ -1,11 +1,12 @@
 #' Create country summary report
 #'
 #' @param gf_optimised Optimised data for a country
-#' @param gts Country level annual summary from the GTS for a country
+#' @param gf_gp Country level annual summary from the GF runs of global plan
+#' @param gts_gp Country level annual summary from the GTS global plan
 #' @param output_file Report output address
 #'
 #' @export
-render_report <- function(gf_optimised, gts, output_file) {
+render_report <- function(gf_optimised, gf_gp, gts_gp, output_file) {
   rmd_file <- system.file("country_report.Rmd", package = "gf")
   rmarkdown::render(
     rmd_file, 
