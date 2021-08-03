617334a711040b97b889f3e1311405891b346b04
Peter Winskill <p.winskill@imperial.ac.uk>
2021-08-03 16:03:02 +0100

    Remove discounting from objective function

--- R/optimisation.R
+++ R/optimisation.R
@@ -2,18 +2,16 @@
 #'
 #' @param processed_output Processed model output
 #' @param max_year Maximum year to quantify impact to
-#' @param discounting_rate rate Rate outputs discounted after the replenishment period
 #'
 #' @return Data for use in optimisation
 #' @export
-create_optimisation_data <- function(processed_output, max_year = 2030, discounting_rate = 0.03){
+create_optimisation_data <- function(processed_output, max_year = 2030){
   processed_output %>%
     dplyr::filter(.data$post == "continue",
                   .data$year %in% 2024:max_year) %>%
-    dplyr::mutate(discounting = ifelse(.data$year %in% 2024:2026, 1, (1 + discounting_rate) ^ (.data$year - 2026))) %>%
     dplyr::group_by(.data$NAME_0, .data$NAME_1, .data$NAME_2, .data$ur, .data$pre, .data$replenishment) %>%
-    dplyr::summarise(cases = sum(.data$cases / .data$discounting),
-                     deaths = sum(.data$deaths / .data$discounting),
+    dplyr::summarise(cases = sum(.data$cases),
+                     deaths = sum(.data$deaths),
                      cost = sum(.data$total_cost)) %>%
     dplyr::ungroup() %>%
     dplyr::mutate(y = (.data$cases * (sum(.data$deaths) / sum(.data$cases))) + .data$deaths)
