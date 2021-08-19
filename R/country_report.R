bce4a7cd6d6d9e853e63aa6d28f2ff4bbaff42c1
Peter Winskill <p.winskill@imperial.ac.uk>
2021-08-19 12:09:11 +0100

    Add budget check to country report

--- R/country_report.R
+++ R/country_report.R
@@ -3,17 +3,19 @@
 #' @param gf_optimised Optimised data for a country
 #' @param gf_gp Country level annual summary from the GF runs of global plan
 #' @param gts_gp Country level annual summary from the GTS global plan
+#' @param global_plan_budget Replenishment period budget from the global plan
 #' @param output_file Report output address
 #'
 #' @export
-render_report <- function(gf_optimised, gf_gp, gts_gp, output_file) {
+render_report <- function(gf_optimised, gf_gp, gts_gp, global_plan_budget, output_file) {
   rmd_file <- system.file("country_report.Rmd", package = "gf")
   rmarkdown::render(
     rmd_file, 
     params = list(
       gf_optimised = gf_optimised,
       gf_gp = gf_gp,
-      gts_gp = gts_gp
+      gts_gp = gts_gp,
+      global_plan_budget = global_plan_budget
     ),
     output_file = output_file
   )
