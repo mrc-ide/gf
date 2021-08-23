6a99374f9e23ca638ee699b8186d4b569dcd7cf1
Peter Winskill <p.winskill@imperial.ac.uk>
2021-08-23 10:58:37 +0100

    Update report budget levels

--- R/country_report.R
+++ R/country_report.R
@@ -5,9 +5,10 @@
 #' @param gts_gp Country level annual summary from the GTS global plan
 #' @param global_plan_budget Replenishment period budget from the global plan
 #' @param output_file Report output address
+#' @param budget_levels Budget increments used
 #'
 #' @export
-render_report <- function(gf_optimised, gf_gp, gts_gp, global_plan_budget, output_file) {
+render_report <- function(gf_optimised, gf_gp, gts_gp, global_plan_budget, output_file, budget_levels = paste(seq(0.05, 1, 0.05))) {
   rmd_file <- system.file("country_report.Rmd", package = "gf")
   rmarkdown::render(
     rmd_file, 
@@ -15,7 +16,8 @@ render_report <- function(gf_optimised, gf_gp, gts_gp, global_plan_budget, outpu
       gf_optimised = gf_optimised,
       gf_gp = gf_gp,
       gts_gp = gts_gp,
-      global_plan_budget = global_plan_budget
+      global_plan_budget = global_plan_budget,
+      budget_levels = budget_levels
     ),
     output_file = output_file
   )
