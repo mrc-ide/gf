5351d841d0b2f4de617c463a147a99656e2624ac
Peter Winskill <p.winskill@imperial.ac.uk>
2021-08-24 10:37:59 +0100

    Fixed scenario diagnostic plot in report

--- R/country_report.R
+++ R/country_report.R
@@ -4,11 +4,12 @@
 #' @param gf_gp Country level annual summary from the GF runs of global plan
 #' @param gts_gp Country level annual summary from the GTS global plan
 #' @param global_plan_budget Replenishment period budget from the global plan
+#' @param gf_fixed Fixed scenario output
 #' @param output_file Report output address
 #' @param budget_levels Budget increments used
 #'
 #' @export
-render_report <- function(gf_optimised, gf_gp, gts_gp, global_plan_budget, output_file, budget_levels = paste(seq(0.05, 1, 0.05))) {
+render_report <- function(gf_optimised, gf_gp, gts_gp, global_plan_budget, gf_fixed, output_file, budget_levels = paste(seq(0.05, 1, 0.05))) {
   rmd_file <- system.file("country_report.Rmd", package = "gf")
   rmarkdown::render(
     rmd_file, 
@@ -17,6 +18,7 @@ render_report <- function(gf_optimised, gf_gp, gts_gp, global_plan_budget, outpu
       gf_gp = gf_gp,
       gts_gp = gts_gp,
       global_plan_budget = global_plan_budget,
+      gf_fixed = gf_fixed,
       budget_levels = budget_levels
     ),
     output_file = output_file
