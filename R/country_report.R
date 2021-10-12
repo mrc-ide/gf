11e302cd246b5994e328da6298421560bfd2be1a
Peter Winskill <p.winskill@imperial.ac.uk>
2021-10-12 11:33:15 +0100

    Pre-checks in report

--- R/country_report.R
+++ R/country_report.R
@@ -5,11 +5,12 @@
 #' @param gts_gp Country level annual summary from the GTS global plan
 #' @param global_plan_budget Replenishment period budget from the global plan
 #' @param gf_fixed Fixed scenario output
+#' @param comp Comparison with pre-inputs
 #' @param output_file Report output address
 #' @param budget_levels Budget increments used
 #'
 #' @export
-render_report <- function(gf_optimised, gf_gp, gts_gp, global_plan_budget, gf_fixed, output_file, budget_levels = paste(seq(0.05, 1, 0.05))) {
+render_report <- function(gf_optimised, gf_gp, gts_gp, global_plan_budget, gf_fixed, comp, output_file, budget_levels = paste(seq(0.05, 1, 0.05))) {
   rmd_file <- system.file("country_report.Rmd", package = "gf")
   rmarkdown::render(
     rmd_file, 
@@ -19,6 +20,7 @@ render_report <- function(gf_optimised, gf_gp, gts_gp, global_plan_budget, gf_fi
       gts_gp = gts_gp,
       global_plan_budget = global_plan_budget,
       gf_fixed = gf_fixed,
+      comp = comp,
       budget_levels = budget_levels
     ),
     output_file = output_file
